4445b6cedcaefcaa101d9f7ec965cc37da15fdbc
lg641135360 <641135360@qq.com>
2017-11-11 18:01:26 +0800

    add aspect

---
+++ src/main/java/com/lg/cloud_note/aspect/LoggerBean.java
@@ -0,0 +1,10 @@
+package com.lg.cloud_note.aspect;
+
+/**
+ * Create by MIO on 2017/11/11 17:49
+ */
+public class LoggerBean {
+    public void logController(){
+        System.out.println("AOP功能注入!");
+    }
+}
